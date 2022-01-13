c8f95f8df0062d53ac25646e2592882d08b7cdb4
Troy Lee <troy@infinitytechnology.io>
2022-01-13 08:53:52 +0700

    Complete question 2

--- lib/utils/buyer_combination_util.dart
+++ lib/utils/buyer_combination_util.dart
@@ -1,5 +1,30 @@
-class BuyerCombinationUtil {
-  List<List<int>> combinations = [];
+import 'package:flutter/foundation.dart';
 
-  void listOutBuyerCombinations(int buyerAmount) {}
+class BuyerCombinationUtil {
+  List<List<int>> listOutBuyerCombinations(
+      int buyerAmount,
+      List<int> possiblePurchaseAmounts,
+      List<int> history,
+      List<List<int>> combinations) {
+    try {
+      if (buyerAmount == 0) {
+        combinations.add(history.toList());
+        return combinations;
+      }
+
+      for (int possiblePurchaseAmount in possiblePurchaseAmounts) {
+        if (buyerAmount - possiblePurchaseAmount >= 0) {
+          List<int> purchaseAmounts = history.toList();
+          purchaseAmounts.add(possiblePurchaseAmount);
+
+          listOutBuyerCombinations(buyerAmount - possiblePurchaseAmount,
+              possiblePurchaseAmounts, purchaseAmounts, combinations);
+        }
+      }
+
+      return combinations.toSet().toList();
+    } catch (error) {
+      return [];
+    }
+  }
 }
